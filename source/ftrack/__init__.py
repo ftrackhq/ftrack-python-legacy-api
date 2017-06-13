06f46ae3a1b476d2ee87a43e58a8130c08b8cf10
Eric Hermelin <erich@cinesite.com>
2017-06-13 14:50:23 +0200

    honor legacy FTRACK_PROXY environment variable

--- source/ftrack/__init__.py
+++ source/ftrack/__init__.py
@@ -9,6 +9,13 @@ import sys
 os.environ.setdefault('FTRACK_SERVER', 'https://YOUR-FTRACK-SERVER')
 os.environ.setdefault('FTRACK_APIKEY', 'YOUR-API-KEY')
 
+# Honor the legacy FTRACK_PROXY environment variable.
+if 'FTRACK_PROXY' in os.environ and os.environ.get('FTRACK_PROXY'):
+    for variable in ('http_proxy', 'https_proxy'):
+        os.environ.setdefault(
+            variable, os.environ.get('FTRACK_PROXY')
+        )
+
 # Add ftrack core egg to path.
 sys.path.append(
     os.path.join(
