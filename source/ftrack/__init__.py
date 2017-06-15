b28241e72f6b513e1b4fee34772b2d1306b8a3e4
Eric Hermelin <erich@cinesite.com>
2017-06-15 14:30:23 +0200

    if https_proxy and/or http_proxy exists but not FTRACK_PROXY, update FTRACK_PROXY

--- source/ftrack/__init__.py
+++ source/ftrack/__init__.py
@@ -10,10 +10,24 @@ os.environ.setdefault('FTRACK_SERVER', 'https://YOUR-FTRACK-SERVER')
 os.environ.setdefault('FTRACK_APIKEY', 'YOUR-API-KEY')
 
 # Honor the legacy FTRACK_PROXY environment variable.
+
+_proxy_variables = (
+    'https_proxy',
+    'http_proxy'
+)
+
 if 'FTRACK_PROXY' in os.environ and os.environ.get('FTRACK_PROXY'):
-    for variable in ('http_proxy', 'https_proxy'):
+    for variable in _proxy_variables:
         os.environ.setdefault(
             variable, os.environ.get('FTRACK_PROXY')
+        )
+
+elif set(_proxy_variables).intersection(os.environ):
+    # If http_proxy and / or https_proxy environment variables are set
+    # update FTRACK_PROXY with the value. If both exist https_proxy win's.
+    for variable in [variable for variable in _proxy_variables if os.environ.get(variable)]:
+        os.environ.setdefault(
+            'FTRACK_PROXY', os.environ.get(variable)
         )
 
 # Import core ftrack functionality into top level namespace.
